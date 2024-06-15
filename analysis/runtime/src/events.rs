51d52e1ecfa78e763fdb94bb698be176d7d5288a
Andrei Homescu <ah@immunant.com>
2024-06-14 19:38:44 -0700

    Add size of locals to AddrOfLocal events
    Keep track of the size of every local for the
    new provenance algorithm.

--- analysis/runtime/src/events.rs
+++ analysis/runtime/src/events.rs
@@ -60,7 +60,7 @@ pub enum EventKind {
     StoreAddrTaken(Pointer),
 
     /// The pointer that appears as the address result of addr_of(Local)
-    AddrOfLocal(Pointer, Local),
+    AddrOfLocal(Pointer, Local, u32),
 
     /// Casting the pointer to an int
     ToInt(Pointer),
@@ -105,7 +105,7 @@ impl Debug for EventKind {
             StoreAddr(ptr) => write!(f, "store(0x{:x})", ptr),
             StoreAddrTaken(ptr) => write!(f, "store(0x{:x})", ptr),
             CopyRef => write!(f, "copy_ref"),
-            AddrOfLocal(ptr, _) => write!(f, "addr_of_local = 0x{:x}", ptr),
+            AddrOfLocal(ptr, _, size) => write!(f, "addr_of_local({}) = 0x{:x}", size, ptr),
             ToInt(ptr) => write!(f, "to_int(0x{:x})", ptr),
             FromInt(ptr) => write!(f, "from_int(0x{:x})", ptr),
             LoadValue(ptr) => write!(f, "load_value(0x{:x})", ptr),
